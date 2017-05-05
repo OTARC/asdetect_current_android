c22ca865a42836fde194e65d7a84bdce80648f4c
Lachlan James Burnside <l.burnside@latrobe.edu.au>
2017-05-05 15:49:58 +1000

    no message

--- client/js/auth.js
+++ client/js/auth.js
@@ -309,7 +309,7 @@ angular.module('asdetect.auth', ['openfb', 'asdetect.config', 'asdetect.interact
                 .success(function (data) {
                      //$ionicPopup.alert({title:  'Reset Password', content: "Password reset!"});
                      
-                    var urlParamEmail = document.location.href.substr(document.location.href.lastInd‌​exOf('/') + 1);
+                    var urlParamEmail = $location.path().split('/').pop();
                     console.log('urlParamEmail:'+urlParamEmail);
 
                     if (window.location.hostname.startsWith("staging")){
