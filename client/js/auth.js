0ef10259b8498ba651f2181923d5d0ff4cbf98bc
Lachlan James Burnside <l.burnside@latrobe.edu.au>
2017-05-05 14:30:35 +1000

    no message

--- client/js/auth.js
+++ client/js/auth.js
@@ -308,7 +308,7 @@ angular.module('asdetect.auth', ['openfb', 'asdetect.config', 'asdetect.interact
             Auth.resetpassword($scope.user)
                 .success(function (data) {
                      //$ionicPopup.alert({title:  'Reset Password', content: "Password reset!"});
-                     window.open('https://otarc--dev--c.cs17.visual.force.com/apex/asdetectevalstudyv2_optinemailsent');
+                     window.open('https://otarc--dev--c.cs17.visual.force.com/apex/asdetectevalstudyv2_optinemailsent,\'_self\'');
                 })
                 .error(function () {
                             $ionicPopup.alert({title: 'Set Password', content: "Problem"});
