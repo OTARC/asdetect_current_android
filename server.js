e9788c1bc0de43b268bef5e1bd08b1721a3721e8
ASDetect - Olga Tennison Autism Research Centre <36247257+OTARC@users.noreply.github.com>
2018-02-08 15:48:27 +1100

    Update server.js

--- server.js
+++ server.js
@@ -133,4 +133,6 @@ app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
-
+server.listen(config.port, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
